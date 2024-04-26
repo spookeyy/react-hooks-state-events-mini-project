f4e5249383a1d597f5b54725d2d51c3a1abf0128
spookeyy <pangasmeshack@gmail.com>
2024-04-26 21:37:14 +0300

    task complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({ text, task, category, click }) {
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
+      {/* <div className="label">CATEGORY HERE</div>
       <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <button className="delete">X</button> */}
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={() => click(task)}>
+        X
+      </button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks, deleteTask , click}) {
+  const tasksList = tasks.map((task, index) =>
+  <Task
+    key={index}
+    task={task}
+    text={task.text}
+    category={task.category}
+    click={click}
+  />)
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
+    <div className="tasks">{tasksList}
     </div>
   );
 }
