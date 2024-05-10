517f264ec0266723af9ba97acf81ab6f599412e9
spookeyy <pangasmeshack@gmail.com>
2024-05-10 12:53:42 +0300

    test pass

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,20 +1,17 @@
 import React from "react";
 import Task from "./Task";
 
-
-function TaskList({tasks, deleteTask , click}) {
-  const tasksList = tasks.map((task, index) =>
-  <Task
-    key={index}
-    task={task}
-    text={task.text}
-    category={task.category}
-    click={click}
-  />)
-  return (
-    <div className="tasks">{tasksList}
-    </div>
-  );
+function TaskList({ tasks, deleteTask, click }) {
+  const tasksList = tasks.map((task, ind) => (
+    <Task
+      key={ind}
+      task={task}
+      text={task.text}
+      category={task.category}
+      click={click}
+    />
+  ));
+  return <div className="tasks">{tasksList}</div>;
 }
 
 export default TaskList;
